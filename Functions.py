244e5e7acdf395e492191bf3c7ccd5976cdd6aa5
JackBidwell <jackbidwell2002@gmail.com>
2024-06-20 14:59:12 -0500

    Speed based on RPM, and motorTorque

--- Functions.py
+++ Functions.py
@@ -4,9 +4,19 @@ def circumfrance(radius):
   return circumfrance
 
 def GearRaitio(input, output):
+  # The gear ratio is the is the ratio of number of teeth on the output gear (connected to the wheel) to the input gear (connected to the motor) the number of teeth is directly proportional to the circumfrance of these gears.
   gearRatio = output/input
   return gearRatio
 
 def WheelTorque(MotorTorque, gearRatio):
+  # Wheeltorque is calculated by the torque on the input gear (the gear connected to the motor) multiplied by the gear ratio
   wheelTorque = MotorTorque*gearRatio
   return wheelTorque
+
+def MotorTorque(wheelTorque, input, output):
+  # You can find the torque being applied to the input (motor gear) by multiplying the wheel torque by the inverse of the gear ratio
+  motorTorque = wheelTorque * (input/output)
+
+def Speed(tireCircumfrance, outputRPM):
+  # The output RPM, how many times the tire is rotating per minute * 60 will give you a speed, the unit is based on how you entered the rest of the information.
+  speed = tireCircumfrance*outputRPM*60
