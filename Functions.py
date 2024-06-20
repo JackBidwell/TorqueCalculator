20e3070b4faa2bf656f405bdf21af4197e4acc82
JackBidwell <jackbidwell2002@gmail.com>
2024-06-20 13:46:26 -0500

    Circumfrance geargratio and wheeltorque

--- Functions.py
+++ Functions.py
@@ -2,3 +2,11 @@ import math
 def circumfrance(radius):
   circumfrance = radius(math.pi)
   return circumfrance
+
+def GearRaitio(input, output):
+  gearRatio = output/input
+  return gearRatio
+
+def WheelTorque(MotorTorque, gearRatio):
+  wheelTorque = MotorTorque*gearRatio
+  return wheelTorque
